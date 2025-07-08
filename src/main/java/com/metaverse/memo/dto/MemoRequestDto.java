b6036e559e7b436adb78f37735d727133f331c14
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-08 16:08:41 +0900

    feat. Memo 요청 및 응답 DTO 생성

---
+++ src/main/java/com/metaverse/memo/dto/MemoRequestDto.java
@@ -0,0 +1,10 @@
+package com.metaverse.memo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+
+}
---
+++ src/main/java/com/metaverse/memo/dto/MemoResponseDto.java
@@ -0,0 +1,10 @@
+package com.metaverse.memo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoResponseDto {
+    private Long id;
+    private String username;
+    private String contents;
+}
