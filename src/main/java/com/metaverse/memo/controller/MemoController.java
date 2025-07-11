03c56257e9b1defe1ad34da8e4134fd82ecf10b2
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-11 16:19:24 +0900

    feat: 3Layer - Controller - Service 분리

--- src/main/java/com/metaverse/memo/controller/MemoController.java
+++ src/main/java/com/metaverse/memo/controller/MemoController.java
@@ -1,20 +1,13 @@
 package com.metaverse.memo.controller;
 
-import com.metaverse.memo.domain.Memo;
+
 import com.metaverse.memo.dto.MemoRequestDto;
 import com.metaverse.memo.dto.MemoResponseDto;
+import com.metaverse.memo.service.MemoService;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -28,90 +21,28 @@ public class MemoController {
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
-        //RequestDto -> Entity 변환
-        Memo memo = new Memo(memoRequestDto);
+        MemoService memoService = new MemoService(jdbcTemplate);
 
-        //DB저장
-        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환 받기 위한 객체
-
-        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
-        jdbcTemplate.update(con -> {
-            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.setString(1, memo.getUsername());
-            preparedStatement.setString(2, memo.getContents());
-            return preparedStatement;
-        }, keyHolder);
-
-        //DB INSERT 후 받아온 기본 키 확인
-        Long id = keyHolder.getKey().longValue();
-        memo.setId(id);
-
-        //Entity -> ResponseDto 변환
-        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
-        return memoResponseDto;
+        return memoService.createMemo(memoRequestDto);
     }
 
 
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos(){
-        // DB 조회
-        String sql = "SELECT * FROM memo";
-        List<MemoResponseDto> responseList = jdbcTemplate.query(sql,new RowMapper<MemoResponseDto>() {
-           @Override
-           public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException{
-               Long id = rs.getLong("id");
-               String username = rs.getString("username");
-               String contents = rs.getString("contents");
-               return new MemoResponseDto(id,username,contents);
-           }
-        });
+        MemoService memoService = new MemoService(jdbcTemplate);
 
-        return responseList;
+        return memoService.getMemos();
     }
 
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
-        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
-        Memo memo = findById(id);
-
-        //메모 수정
-        if (memo!=null){
-            String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
-            jdbcTemplate.update(sql, memoRequestDto.getUsername(), memoRequestDto.getContents(), id);
-            return id;
-        }else{
-            throw new IllegalArgumentException("해당 ID의 메모는 존재하지 않습니다");
-        }
+        MemoService memoService = new MemoService(jdbcTemplate);
+        return memoService.updateMemo(id,memoRequestDto);
     }
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
-        Memo memo = findById(id);
-
-        // 메모 삭제
-        if (memo!=null){
-            String sql = "DELETE FROM memo WHERE id = ?";
-            jdbcTemplate.update(sql, id);
-            return id;
-        } else{
-            throw new IllegalArgumentException("해당 ID의 메모는 존재하지 않습니다");
-        }
-    }
-
-    private Memo findById(Long id) {
-        // DB 조회
-        String sql = "SELECT * FROM memo WHERE id = ?";
-
-        return jdbcTemplate.query(sql, resultSet -> {
-            if(resultSet.next()){
-                Memo memo = new Memo();
-                memo.setUsername(resultSet.getString("username"));
-                memo.setContents(resultSet.getString("contents"));
-                return memo;
-            }else {
-                return null;
-            }
-        },id);
+        MemoService memoService = new MemoService(jdbcTemplate);
+        return memoService.deleteMemo(id);
     }
 }
---
+++ src/main/java/com/metaverse/memo/service/MemoService.java
@@ -0,0 +1,108 @@
+package com.metaverse.memo.service;
+
+import com.metaverse.memo.domain.Memo;
+import com.metaverse.memo.dto.MemoRequestDto;
+import com.metaverse.memo.dto.MemoResponseDto;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class MemoService {
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemoService(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public MemoResponseDto createMemo(MemoRequestDto memoRequestDto) {
+        //RequestDto -> Entity 변환
+        Memo memo = new Memo(memoRequestDto);
+
+        //DB저장
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환 받기 위한 객체
+
+        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
+        jdbcTemplate.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, memo.getUsername());
+            preparedStatement.setString(2, memo.getContents());
+            return preparedStatement;
+        }, keyHolder);
+
+        //DB INSERT 후 받아온 기본 키 확인
+        Long id = keyHolder.getKey().longValue();
+        memo.setId(id);
+
+        //Entity -> ResponseDto 변환
+        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+        return memoResponseDto;
+    }
+
+    public List<MemoResponseDto> getMemos(){
+        // DB 조회
+        String sql = "SELECT * FROM memo";
+
+        List<MemoResponseDto> responseList = jdbcTemplate.query(sql,new RowMapper<MemoResponseDto>() {
+            @Override
+            public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Long id = rs.getLong("id");
+                String username = rs.getString("username");
+                String contents = rs.getString("contents");
+                return new MemoResponseDto(id,username,contents);
+            }
+        });
+
+        return responseList;
+    }
+
+    public Long updateMemo(Long id, MemoRequestDto memoRequestDto) {
+        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+        Memo memo = findById(id);
+
+        //메모 수정
+        if (memo!=null){
+            String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
+            jdbcTemplate.update(sql, memoRequestDto.getUsername(), memoRequestDto.getContents(), id);
+            return id;
+        }else{
+            throw new IllegalArgumentException("해당 ID의 메모는 존재하지 않습니다");
+        }
+    }
+
+    public Long deleteMemo(Long id){
+        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+        Memo memo = findById(id);
+
+        // 메모 삭제
+        if (memo!=null){
+            String sql = "DELETE FROM memo WHERE id = ?";
+            jdbcTemplate.update(sql, id);
+            return id;
+        } else{
+            throw new IllegalArgumentException("해당 ID의 메모는 존재하지 않습니다");
+        }
+    }
+
+    private Memo findById(Long id) {
+        // DB 조회
+        String sql = "SELECT * FROM memo WHERE id = ?";
+
+        return jdbcTemplate.query(sql, resultSet -> {
+            if(resultSet.next()){
+                Memo memo = new Memo();
+                memo.setUsername(resultSet.getString("username"));
+                memo.setContents(resultSet.getString("contents"));
+                return memo;
+            }else {
+                return null;
+            }
+        },id);
+    }
+}
