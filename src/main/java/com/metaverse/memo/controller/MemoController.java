ce13b0a0cf3c986fb7de735261f0b8c045616950
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-11 15:26:15 +0900

    feat: 메모 목록 수정 기능 JDBC 소스코드(과거)

--- src/main/java/com/metaverse/memo/controller/MemoController.java
+++ src/main/java/com/metaverse/memo/controller/MemoController.java
@@ -69,21 +69,21 @@ public class MemoController {
         return responseList;
     }
 
-//    @PutMapping("/memos/{id}")
-//    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
-//        // 해당 id의 메모가 존재하는지 확인
-//        if(memoList.containsKey(id)){
-//            //해당 메모 가져오기
-//            Memo memo = memoList.get(id);
-//
-//            //메모 수정
-//            memo.update(memoRequestDto);
-//            return memo.getId();
-//        }else{
-//            throw new IllegalArgumentException("Memo not found");
-//        }
-//    }
-//
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
+        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+        Memo memo = findById(id);
+
+        //메모 수정
+        if (memo!=null){
+            String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
+            jdbcTemplate.update(sql, memoRequestDto.getUsername(), memoRequestDto.getContents(), id);
+            return id;
+        }else{
+            throw new IllegalArgumentException("해당 ID의 메모는 존재하지 않습니다");
+        }
+    }
+
 //    @DeleteMapping("/memos/{id}")
 //    public Long deleteMemo(@PathVariable Long id) {
 //        if(memoList.containsKey(id)){
@@ -94,4 +94,19 @@ public class MemoController {
 //            throw new IllegalArgumentException("Memo not found");
 //        }
 //    }
+    private Memo findById(Long id) {
+        // DB 조회
+        String sql = "SELECT * FROM memo WHERE id = ?";
+
+        return jdbcTemplate.query(sql, resultSet -> {
+            if(resultSet.next()){
+                Memo memo = new Memo();
+                memo.setUsername(resultSet.getString("username"));
+                memo.setContents(resultSet.getString("contents"));
+                return memo;
+            }else {
+                return null;
+            }
+        },id);
+    }
 }
